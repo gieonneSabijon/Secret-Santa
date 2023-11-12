d19891559dedb8cb53df987d6ec1a18aff0b61e3
gieonneSabijon <gieonnesabijon@gmail.com>
2023-11-11 22:48:15 -0500

    Implemented Email sending

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template, jsonify, request
 from random import randrange
+from dotenv import load_dotenv
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os
+
+load_dotenv()
+
+appPassword = os.environ.get("APP_PASS")
+emailString = os.environ.get("EMAIL")
 
 app = Flask(__name__)
 
@@ -16,7 +26,10 @@ def handle_request():
         shuffle(recievers)
 
         giftPairs = makePairs(senders, recievers, data)
+        for sender in giftPairs:
+            sendMail(giftPairs[sender], sender, data[sender])
 
+        
         result = {"message": "JSON received", "data": data}
         return jsonify(result)
     
@@ -36,6 +49,31 @@ def shuffle(items):
         i = i - 1
         j = randrange(i)
         items[j], items[i] = items[i], items[j]
+
+def sendMail(giftPair, sender, senderEmail):
+    recieverName = giftPair[0]
+    recieverEmail = giftPair[1]
+
+    
+    message = MIMEMultipart()
+    message["To"] = senderEmail
+    message["From"] = emailString
+    message["Subject"] = 'Secret Santa'
+
+    title = '<b> Secret Santa! </b>'
+    messageText = MIMEText(f'''Hello {sender}! You will be giving your gift to {recieverName}!''','html')
+    message.attach(messageText)
+
+    mailServer = smtplib.SMTP('smtp.gmail.com:587')
+    mailServer.ehlo('Gmail')
+    mailServer.starttls()
+    mailServer.login(emailString, appPassword)
+
+    fromaddr = emailString
+    toaddrs  = senderEmail
+
+    mailServer.sendmail(fromaddr,toaddrs,message.as_string())
+    mailServer.quit()
 
 if __name__ == "__main__":
     app.run(debug = True)
